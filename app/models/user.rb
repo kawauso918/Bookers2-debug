ea2dd0436031224713030d1a1893d83fcf446e80
EC2 Default User <ec2-user@ip-172-31-24-195.ec2.internal>
2021-12-29 08:48:27 +0000

    [フォローフォロワー]

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,28 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
+  has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
+  
   def already_liked?(book)
     self.likes.exists?(book_id: book.id)
+  end
+  
+  # ユーザーをフォローする
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  # フォローしていればtrueを返す
+  def following?(user)
+    following_user.include?(user)
   end
   
   attachment :profile_image
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   get '/home/about' => 'homes#about'
   
-  resources :users, only: [:show,:index,:edit,:update] 
+  resources :users, only:[:index, :show, :edit, :update] do
+    member do
+    get :follows, :followers
+  end
+    resource :relationships, only: [:create, :destroy]
+  end
+  
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
